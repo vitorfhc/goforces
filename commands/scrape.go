8138b56e2397e4019a87025b3493157ffd6af28c
Vitor Falcao <vitorfhc@protonmail.com>
2020-02-05 16:10:05 -0300

    Scrape scrape scrape

--- commands/scrape.go
+++ commands/scrape.go
@@ -1,21 +1,61 @@
 package commands
 
 import (
-	"fmt"
+	"log"
 
 	"github.com/spf13/cobra"
 )
 
+type target struct {
+	contest   string
+	problem   string
+	isContest bool
+}
+
 var scrapeCmd = &cobra.Command{
-	Use:   "scrape [url]",
-	Short: "Scrapes the problems from a url",
-	Long: `Scrapes all the problems from a contest
-or just the specific problem`,
-	Args: cobra.ExactArgs(1),
+	Use:   "scrape [contest] [problem]",
+	Short: "Scrapes a contest or a problem",
+	Long: `Scrapes all the problems from a contest if no problem is given
+or just the specific problem.
+Contest: saves each problem in a specific folder
+Problem: saves every inputs, outputs, and the problem's text`,
+	Args: cobra.RangeArgs(1, 2),
 	Run:  execute,
 }
 
 func execute(cmd *cobra.Command, args []string) {
-	url := args[0]
-	fmt.Println("Scrapping", url)
+	t := target{"", "", false}
+	t.contest = args[0]
+
+	if len(args) == 1 {
+		t.isContest = true
+		scrape(t)
+		return
+	}
+
+	t.problem = args[1]
+	scrape(t)
+}
+
+func scrape(t target) {
+	if t.isContest {
+		log.Println("Scrapping contest", t.contest)
+		problems := scrapeContest(t)
+
+		for _, pt := range problems {
+			scrapeProblem(pt)
+		}
+
+		return
+	}
+
+	scrapeProblem(t)
+}
+
+func scrapeContest(t target) []target {
+	return nil
+}
+
+func scrapeProblem(t target) {
+	log.Println("Scrapping problem", t.problem, "from contest", t.contest)
 }
